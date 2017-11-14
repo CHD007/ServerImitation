ba56d3c26a4e41acfbdb784e587efadd6a51f71e
chd007 <chernyshov777@gmail.com>
2017-11-14 21:05:52 +0300

    movToPosTest

--- test/objects/PlayersTests.java
+++ test/objects/PlayersTests.java
@@ -1,4 +1,19 @@
 package objects;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
 public class PlayersTests {
+    private static final Logger LOGGER = Logger.getLogger(PlayersTests.class.getName());
+
+    @Test
+    public void movToPosTest() {
+        Player player = new Player();
+        player.setPosX(0);
+        player.setPosY(0);
+        Action action = player.movToPos(new FieldObject(10, 10));
+        Assert.assertEquals("turn", action.getActionType());
+    }
 }
