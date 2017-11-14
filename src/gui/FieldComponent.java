581d4006b16090c51312dbf0dcd588a71e52ed93
chd007 <chernyshov777@gmail.com>
2017-11-14 20:24:40 +0300

    customize football field

--- src/gui/FieldComponent.java
+++ src/gui/FieldComponent.java
@@ -28,12 +28,16 @@ public class FieldComponent extends JPanel {
     public static final double ballShapeWidth = 15;
     public static final double ballShapeHeight = 15;
 
+    public FieldComponent() {
+        setBackground(new Color(48, 226, 76));
+    }
+
     /**
      * Add a player to the component.
+     *
      * @param player the player to add
      */
-    public void addPlayer(Player player)
-    {
+    public void addPlayer(Player player) {
         if (players == null) {
             players = new ArrayList<Player>();
         }
@@ -43,18 +47,17 @@ public class FieldComponent extends JPanel {
 
     /**
      * Add a ball to the component.
+     *
      * @param ball the player to add
      */
-    public void addBall(Ball ball)
-    {
+    public void addBall(Ball ball) {
         this.ball = ball;
     }
 
     /**
      * Gets the shape of the player at its current position.
      */
-    public Ellipse2D getPlayerShape(Player player)
-    {
+    public Ellipse2D getPlayerShape(Player player) {
         return new Ellipse2D.Double(getPlayerShapePosX(player),
                 getPlayerShapePosY(player), playerShapeWidth, playerShapeHeight);
     }
@@ -62,18 +65,41 @@ public class FieldComponent extends JPanel {
     /**
      * Gets the shape of the ball at its current position.
      */
-    public Ellipse2D getBallShape()
-    {
+    public Ellipse2D getBallShape() {
         return new Ellipse2D.Double(getBallShapePosX(),
                 getBallShapePosY(), ballShapeWidth, ballShapeHeight);
     }
 
-    public void paintComponent(Graphics g)
-    {
+    private void drawFootballField(Graphics g) {
+        // center line
+        g.drawLine(DEFAULT_WIDTH / 2, 0, DEFAULT_WIDTH / 2, DEFAULT_HEIGHT);
+
+        // center circle
+        g.drawOval(DEFAULT_WIDTH / 2 - 35, DEFAULT_HEIGHT / 2 - 35, 70, 70);
+
+        // left goalie zone
+        g.drawLine(0, new Double(14 * scale).intValue(), new Double(17 * scale).intValue(),
+                new Double(14 * scale).intValue());
+        g.drawLine(0, new Double(DEFAULT_HEIGHT - 14 * scale).intValue(),
+                new Double(17 * scale).intValue(), new Double(DEFAULT_HEIGHT - 14 * scale).intValue());
+        g.drawLine(new Double(17 * scale).intValue(),
+                new Double(14 * scale).intValue(), new Double(17 * scale).intValue(), new Double(DEFAULT_HEIGHT - 14 * scale).intValue());
+
+        // right goalie zone
+        g.drawLine(DEFAULT_WIDTH, new Double(14 * scale).intValue(), new Double(DEFAULT_WIDTH - 17 * scale).intValue(),
+                new Double(14 * scale).intValue());
+        g.drawLine(DEFAULT_WIDTH, new Double(DEFAULT_HEIGHT - 14 * scale).intValue(),
+                new Double(DEFAULT_WIDTH - 17 * scale).intValue(), new Double(DEFAULT_HEIGHT - 14 * scale).intValue());
+        g.drawLine(new Double(DEFAULT_WIDTH - 17 * scale).intValue(),
+                new Double(14 * scale).intValue(), new Double(DEFAULT_WIDTH - 17 * scale).intValue(), new Double(DEFAULT_HEIGHT - 14 * scale).intValue());
+    }
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g); // erase background
+        drawFootballField(g);
         Graphics2D g2 = (Graphics2D) g;
         //сглаживание фигуры при рисовании
-        for (Player player: players) {
+        for (Player player : players) {
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             //градиет для игрока, чтобы определить, где лицо
             Color playerColor = Color.black;
@@ -98,22 +124,29 @@ public class FieldComponent extends JPanel {
         g2.draw(getBallShape());
     }
 
-    public Dimension getPreferredSize() { return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT); }
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
 
     public double getPlayerShapePosX(Player player) {
-        return player.getPosX()*scale-playerShapeWidth/2 + DEFAULT_WIDTH/2;
+        return player.getPosX() * scale - playerShapeWidth / 2 + DEFAULT_WIDTH / 2;
     }
 
     public double getPlayerShapePosY(Player player) {
-        return player.getPosY()*scale-playerShapeHeight/2 + DEFAULT_HEIGHT/2;
+        return player.getPosY() * scale - playerShapeHeight / 2 + DEFAULT_HEIGHT / 2;
     }
 
-    public double getBallShapePosX() {return ball.getPosX()*scale-ballShapeWidth/2 + DEFAULT_WIDTH/2;}
+    public double getBallShapePosX() {
+        return ball.getPosX() * scale - ballShapeWidth / 2 + DEFAULT_WIDTH / 2;
+    }
 
-    public double getBallShapePosY() {return ball.getPosY()*scale-ballShapeHeight/2 + DEFAULT_HEIGHT/2;}
+    public double getBallShapePosY() {
+        return ball.getPosY() * scale - ballShapeHeight / 2 + DEFAULT_HEIGHT / 2;
+    }
 
     /**
      * Вычисление координат первой точки для отрисовки градиента
+     *
      * @return точка (тыльная сторона игрока)
      */
     public Point2D getPointP1(Player player) {
@@ -123,22 +156,19 @@ public class FieldComponent extends JPanel {
         if (angle >= 0) {
             if (angle > 90) {
                 angle = 180 - angle;
-                posX = player.getPosX()*scale+ DEFAULT_WIDTH/2 + playerShapeWidth/2*Math.cos(Math.toRadians(-angle));
-                posY = player.getPosY()*scale+ DEFAULT_HEIGHT/2 + playerShapeWidth/2*Math.sin(Math.toRadians(-angle));
+                posX = player.getPosX() * scale + DEFAULT_WIDTH / 2 + playerShapeWidth / 2 * Math.cos(Math.toRadians(-angle));
+                posY = player.getPosY() * scale + DEFAULT_HEIGHT / 2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(-angle));
+            } else {
+                posX = player.getPosX() * scale + DEFAULT_WIDTH / 2 - playerShapeWidth / 2 * Math.cos(Math.toRadians(-angle));
+                posY = player.getPosY() * scale + DEFAULT_HEIGHT / 2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(-angle));
             }
-            else {
-                posX = player.getPosX()*scale+ DEFAULT_WIDTH/2 - playerShapeWidth/2*Math.cos(Math.toRadians(-angle));
-                posY = player.getPosY()*scale+ DEFAULT_HEIGHT/2 + playerShapeWidth/2*Math.sin(Math.toRadians(-angle));
-            }
-        }
-        else {
+        } else {
             if (player.getGlobalBodyAngle() < -90) {
-                posX = player.getPosX()*scale+ DEFAULT_WIDTH/2 + playerShapeWidth/2*Math.cos(Math.toRadians(180+angle));
-                posY = player.getPosY()*scale+ DEFAULT_HEIGHT/2 + playerShapeWidth/2*Math.sin(Math.toRadians(180+angle));
-            }
-            else {
-                posX = player.getPosX()*scale+ DEFAULT_WIDTH/2 - playerShapeWidth/2*Math.cos(Math.toRadians(-angle));
-                posY = player.getPosY()*scale+ DEFAULT_HEIGHT/2 + playerShapeWidth/2*Math.sin(Math.toRadians(-angle));
+                posX = player.getPosX() * scale + DEFAULT_WIDTH / 2 + playerShapeWidth / 2 * Math.cos(Math.toRadians(180 + angle));
+                posY = player.getPosY() * scale + DEFAULT_HEIGHT / 2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(180 + angle));
+            } else {
+                posX = player.getPosX() * scale + DEFAULT_WIDTH / 2 - playerShapeWidth / 2 * Math.cos(Math.toRadians(-angle));
+                posY = player.getPosY() * scale + DEFAULT_HEIGHT / 2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(-angle));
             }
         }
         return new Point2D.Double(posX, posY);
@@ -146,6 +176,7 @@ public class FieldComponent extends JPanel {
 
     /**
      * Вычисление координат второй точки для отрисовки градиента
+     *
      * @return точка (передная сторона игрока)
      */
     public Point2D getPointP2(Player player) {
@@ -155,19 +186,19 @@ public class FieldComponent extends JPanel {
         if (angle >= 0) {
             if (angle > 90) {
                 angle = 180 - angle;
-                posX = player.getPosX()*scale+ DEFAULT_WIDTH/2 - playerShapeWidth / 2 * Math.cos(Math.toRadians(angle));
-                posY = player.getPosY()*scale+ DEFAULT_HEIGHT/2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(angle));
+                posX = player.getPosX() * scale + DEFAULT_WIDTH / 2 - playerShapeWidth / 2 * Math.cos(Math.toRadians(angle));
+                posY = player.getPosY() * scale + DEFAULT_HEIGHT / 2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(angle));
             } else {
-                posX = player.getPosX()*scale+ DEFAULT_WIDTH/2 + playerShapeWidth / 2 * Math.cos(Math.toRadians(angle));
-                posY = player.getPosY()*scale+ DEFAULT_HEIGHT/2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(angle));
+                posX = player.getPosX() * scale + DEFAULT_WIDTH / 2 + playerShapeWidth / 2 * Math.cos(Math.toRadians(angle));
+                posY = player.getPosY() * scale + DEFAULT_HEIGHT / 2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(angle));
             }
         } else {
             if (player.getGlobalBodyAngle() < -90) {
-                posX = player.getPosX()*scale+ DEFAULT_WIDTH/2 - playerShapeWidth / 2 * Math.cos(Math.toRadians(-180 - angle));
-                posY = player.getPosY()*scale+ DEFAULT_HEIGHT/2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(-180 - angle));
+                posX = player.getPosX() * scale + DEFAULT_WIDTH / 2 - playerShapeWidth / 2 * Math.cos(Math.toRadians(-180 - angle));
+                posY = player.getPosY() * scale + DEFAULT_HEIGHT / 2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(-180 - angle));
             } else {
-                posX = player.getPosX()*scale+ DEFAULT_WIDTH/2 + playerShapeWidth / 2 * Math.cos(Math.toRadians(angle));
-                posY = player.getPosY()*scale+ DEFAULT_HEIGHT/2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(angle));
+                posX = player.getPosX() * scale + DEFAULT_WIDTH / 2 + playerShapeWidth / 2 * Math.cos(Math.toRadians(angle));
+                posY = player.getPosY() * scale + DEFAULT_HEIGHT / 2 + playerShapeWidth / 2 * Math.sin(Math.toRadians(angle));
             }
         }
         return new Point2D.Double(posX, posY);
