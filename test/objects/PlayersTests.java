e0455ad949a61c266d1b31708cc2884041b8f252
chd007 <chernyshov777@gmail.com>
2017-11-14 21:31:41 +0300

    outplayingBackOpponent test

--- test/objects/PlayersTests.java
+++ test/objects/PlayersTests.java
@@ -1,19 +1,35 @@
 package objects;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import utils.AdditionalActionParameters;
 
 import java.util.logging.Logger;
 
 public class PlayersTests {
     private static final Logger LOGGER = Logger.getLogger(PlayersTests.class.getName());
+    private Player player;
+
+    @Before
+    public void initPlayer() {
+        player = new Player();
+        player.setGlobalBodyAngle(0);
+        player.setPosX(0);
+        player.setPosY(0);
+        player.setCommand(Command.OUR);
+    }
+
 
     @Test
     public void movToPosTest() {
-        Player player = new Player();
-        player.setPosX(0);
-        player.setPosY(0);
         Action action = player.movToPos(new FieldObject(10, 10));
         Assert.assertEquals("turn", action.getActionType());
+    }
+
+    @Test
+    public void outplayingOpponentBackActionTest() {
+        Action outplayingBackAction = player.outplayingOpponent(new MobileObject(), AdditionalActionParameters.BACK);
+        Assert.assertEquals(player.movToPos(new FieldObject(player.getPosX() - 5, player.getPosY())), outplayingBackAction);
     }
 }
