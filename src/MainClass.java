c866767636edcebffb802cfd62edd47c5668068d
dchernyshov <chernyshov.daniil@nicetu.spb.ru>
2017-12-20 17:17:27 +0300

    refactoring

--- src/MainClass.java
+++ src/MainClass.java
@@ -8,15 +8,12 @@ import java.awt.*;
  */
 public class MainClass {
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-                                   @Override
-                                   public void run() {
-                                       JFrame frame = new MainFrame();
-                                       frame.setTitle("Server Imitation");
-                                       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                                       frame.setVisible(true);
-                                   }
-                               }
+        EventQueue.invokeLater(() -> {
+                    JFrame frame = new MainFrame();
+                    frame.setTitle("Server Imitation");
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setVisible(true);
+                }
         );
     }
 }
